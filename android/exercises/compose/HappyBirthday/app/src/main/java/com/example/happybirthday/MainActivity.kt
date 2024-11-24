05602ed07282f08ff30230142e976491bce5da40
Raphael Rosenberg <raphrosenberg.github@gmail.com>
2024-11-23 23:52:13 -0800

    Finishing up composables tutorial

--- android/exercises/compose/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ android/exercises/compose/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingText(message = "Happy birthday Raph!", from = "From myself")
                 }
             }
         }
@@ -32,21 +33,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp,
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Printin stuff", from = "From Raph")
+        GreetingText(message = "Happy birthday Raph!", from = "From myself")
     }
 }
