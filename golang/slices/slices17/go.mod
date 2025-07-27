6d6b3fc93f3aeddd87cd319264e01e15dea65c08
Raphael Rosenberg <raphrosenberg.github@gmail.com>
2025-07-26 21:20:22 -0700

    Committing slices17

---
+++ golang/slices/slices17/go.mod
@@ -0,0 +1,3 @@
+module protagonist.dev/slices17
+
+go 1.24.2
---
+++ golang/slices/slices17/slices17.go
@@ -0,0 +1,47 @@
+package main
+
+type Message interface {
+	Type() string
+}
+
+type TextMessage struct {
+	Sender  string
+	Content string
+}
+
+func (tm TextMessage) Type() string {
+	return "text"
+}
+
+type MediaMessage struct {
+	Sender    string
+	MediaType string
+	Content   string
+}
+
+func (mm MediaMessage) Type() string {
+	return "media"
+}
+
+type LinkMessage struct {
+	Sender  string
+	URL     string
+	Content string
+}
+
+func (lm LinkMessage) Type() string {
+	return "link"
+}
+
+// Don't touch above this line
+
+func filterMessages(messages []Message, filterType string) []Message {
+	msgs := []Message{}
+
+	for _, m := range messages {
+		if m.Type() == filterType {
+			msgs = append(msgs, m)
+		}
+	}
+	return msgs
+}
